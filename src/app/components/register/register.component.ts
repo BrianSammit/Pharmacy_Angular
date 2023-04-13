9c9199eab28328506361f2a5bdf4c43552f701e4
BrianSammit <briansammit.cruz@gmail.com>
2023-04-13 15:19:31 -0500

    Add register form

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { ToastrService } from 'ngx-toastr';
+import { FirebaseCodeErrorService } from 'src/app/services/firebase-code-error.service';
 
 @Component({
   selector: 'app-register',
@@ -14,24 +17,57 @@ export class RegisterComponent implements OnInit {
 
   constructor(
     private userService: UserService,
-    private router: Router
+    private router: Router,
+    private fb: FormBuilder,
+    private afAuth: AngularFireAuth,
+    private toastr: ToastrService,
+    private firebaseError: FirebaseCodeErrorService
   ) {
-    this.formReg = new FormGroup({
-      email: new FormControl(),
-      password: new FormControl()
+    this.formReg =  this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      repeatPassword: ['', Validators.required],
     })
   }
 
   ngOnInit(): void {
   }
 
-  onSubmit() {
-    this.userService.register(this.formReg.value)
-      .then(response => {
-        console.log(response);
-        this.router.navigate(['/login']);
+   register() {
+    const email = this.formReg.value.email;
+    const password = this.formReg.value.password;
+    const repeatPassword = this.formReg.value.repeatPassword;
+
+    console.log(this.formReg);
+    if (password !== repeatPassword) {
+      this.toastr.error(
+        'The password provided must be equal',
+        'Error'
+      );
+      return;
+    }
+
+    this.afAuth
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        this.verifyEmail();
       })
-      .catch(error => console.log(error));
+      .catch((error) => {
+        this.toastr.error(this.firebaseError.codeError(error.code), 'Error');
+      });
   }
+
+  verifyEmail() {
+    this.afAuth.currentUser
+    .then((user) => user?.sendEmailVerification())
+    .then(() => {
+      this.toastr.info(
+        'We send you a verify email',
+        'Verify email'
+      );
+      this.router.navigate(['/home']);
+    });
+  }
+
 
 }
