9a63b934304c99561f08d9b429d28fc219209124
BrianSammit <briansammit.cruz@gmail.com>
2023-04-13 15:42:03 -0500

    Add if session

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,19 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+  isSession: boolean;
+
   constructor(
+    private afAuth: AngularFireAuth,
     private userService: UserService,
     private router: Router
-  ) { }
+  ) {
+    this.isSession = false;
+  }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.afAuth.authState.subscribe((user) => {
+      this.isSession = !!user;
+    });
   }
 
   onClick() {
